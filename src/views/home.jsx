0cfcf1360b06b8fa1504102211cabf5d1166538a
mohammad-ziadeh <mohammedziadeh6@gmail.com>
2025-07-29 13:38:26 +0300

    cv update

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -6,11 +6,20 @@ import Contact from "../components/partContact";
 export default function Home() {
   return (
     <div>
+      <style>
+        {`
+    @media (max-width: 640px) {
+      .project-title {
+        margin-top: 80vh !important;
+      }
+    }
+  `}
+      </style>
       <PartAbout />
       <div>
         <About />
       </div>
-      <h1 style={{ textAlign: "center", marginTop: "300px" }}>
+      <h1 className="project-title" style={{ textAlign: "center", marginTop: "300px" }}>
         My Biggest Projects
       </h1>
 
